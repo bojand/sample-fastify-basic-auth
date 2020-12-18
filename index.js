354b0ed82eb34ccbd9dcb735ee2aa4b143bbf9bc
Bojan <dbojan@gmail.com>
2020-12-17 21:10:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+const authenticate = { realm: 'Westeros' }
+fastify.register(require('fastify-basic-auth'), { validate, authenticate })
+async function validate (username, password, req, reply) {
+  if (username !== 'Tyrion' || password !== 'wine') {
+    return new Error('Winter is coming')
+  }
+}
+
+fastify.after(() => {
+  fastify.route({
+    method: 'GET',
+    url: '/login',
+    onRequest: fastify.basicAuth,
+    handler: async (req, reply) => {
+      return { hello: 'secret world' }
+    }
+  })
+})
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
